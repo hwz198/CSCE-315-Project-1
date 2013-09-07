223a75d29a807acf20caa868fc8c7c76c57d94c9
heydabop <heydabop@gmail.com>
2013-09-07 15:35:57 -0500

    Show now outputs attributes

--- Database.cpp
+++ Database.cpp
@@ -55,6 +55,12 @@ void Database::Show(string r)
 		return;
 	}
 	//Show the relation in dBase
+        string attribs;
+        for(int i = 0; i < dbase[index].getColumns().size(); ++i){
+          attribs += dbase[index].getColumns()[i].getValue() + "\t";
+        }
+        cout << attribs << endl << endl;
+
 	for(int i=0; i<dbase[index].getRows().size(); ++i)
 	{
 		{
