24c1b0a987cf4ca1509d289869ef036623ffe814
heydabop <heydabop@gmail.com>
2013-09-11 23:59:06 -0500

    Added most tokens and the lexing for them, still very incomplete

--- parser.cpp
+++ parser.cpp
@@ -4,6 +4,195 @@
 #include <cctype>
 using namespace std;
 
+void parser::lex(string input){
+  str = input;
+  t_index = 0;
+  while(t_index < str.size()){
+    if(isspace(get(t_index))){
+      //tokens.push_back(Token(t_index, get(t_index), _space));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == '('){
+      tokens.push_back(Token(t_index, get(t_index), _lparen));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == ')'){
+      tokens.push_back(Token(t_index, get(t_index), _rparen));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == ','){
+      tokens.push_back(Token(t_index, get(t_index), _comma));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == '('){
+      tokens.push_back(Token(t_index, get(t_index), _lparen));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == '<' && get(t_index+1) == '-'){
+      tokens.push_back(Token(t_index, str.substr(t_index, 2), _assign));
+      t_index += 2;
+      continue;
+    }
+    if(get(t_index) == '+'){
+      tokens.push_back(Token(t_index, get(t_index), _plus));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == '-'){
+      tokens.push_back(Token(t_index, get(t_index), _minus));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == '*'){
+      tokens.push_back(Token(t_index, get(t_index), _asterisk));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == ';'){
+      tokens.push_back(Token(t_index, get(t_index), _semicolon));
+      t_index++;
+      continue;
+    }
+    if(get(t_index) == '>'){
+      if(get(t_index+1) == '='){
+        tokens.push_back(Token(t_index, str.substr(t_index, 2), _ge));
+        t_index += 2;
+        continue;
+      } else {
+        tokesn.push_back(Token(t_index, get(t_index), _g));
+        t_index++;
+        continue;
+      }
+    }
+    if(get(t_index) == '<'){
+      if(get(t_index+1) == '='){
+        tokens.push_back(Token(t_index, str.substr(t_index, 2), _le));
+        t_index += 2;
+        continue;
+      } else {
+        tokesn.push_back(Token(t_index, get(t_index), _l));
+        t_index++;
+        continue;
+      }
+    }
+    if(get(t_index) == '!' && get(t_index+1) == '='){
+      tokens.push_back(Token(t_index, str.substr(t_index, 2), _ne));
+      t_index += 2;
+      continue;
+    }
+    if(get(t_index) == '=' && get(t_index+1) == '='){
+      tokens.push_back(Token(t_index, str.substr(t_index, 2), _e));
+      t_index += 2;
+      continue;
+    }
+    if(str.substr(t_index, 7) == "project"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 7), _project));
+      t_index += 7;
+      continue;
+    }
+    if(str.substr(t_index, 6) == "rename"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 6), _rename));
+      t_index += 6;
+      continue;
+    }
+    if(str.substr(t_index, 4) == "OPEN"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 4), _open));
+      t_index += 4;
+      continue;
+    }
+    if(str.substr(t_index, 5) == "CLOSE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 5), _close));
+      t_index += 5;
+      continue;
+    }
+    if(str.substr(t_index, 5) == "WRTIE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 5), _write));
+      t_index += 5;
+      continue;
+    }
+    if(str.substr(t_index, 4) == "EXIT"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 4), _exit));
+      t_index += 4;
+      continue;
+    }
+    if(str.substr(t_index, 4) == "SHOW"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 4), _show));
+      t_index += 4;
+      continue;
+    }
+    if(str.substr(t_index, 6) == "CREATE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 6), _create));
+      t_index += 6;
+      continue;
+    }
+    if(str.substr(t_index, 6) == "UPDATE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 6), _update));
+      t_index += 6;
+      continue;
+    }
+    if(str.substr(t_index, 6) == "INSERT"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 6), _update));
+      t_index += 6;
+      continue;
+    }
+    if(str.substr(t_index, 6) == "DELETE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 6), _delete));
+      t_index += 6;
+      continue;
+    }
+    if(str.substr(t_index, 7) == "VARCHAR"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 7), _varchar));
+      t_index += 7;
+      continue;
+    }
+    if(str.substr(t_index, 5) == "TABLE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 5), _table));
+      t_index += 5;
+      continue;
+    }
+    if(str.substr(t_index, 7) == "PRIMARY"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 7), _primary));
+      t_index += 7;
+      continue;
+    }
+    if(str.substr(t_index, 3) == "KEY"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 3), _key));
+      t_index += 3;
+      continue;
+    }
+    if(str.substr(t_index, 3) == "SET"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 3), _set));
+      t_index += 3;
+      continue;
+    }
+    if(str.substr(t_index, 5) == "WHERE"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 5), _where));
+      t_index += 5;
+      continue;
+    }
+    if(str.substr(t_index, 4) == "INTO"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 4), _into));
+      t_index += 4;
+      continue;
+    }
+    if(str.substr(t_index, 6) == "VALUES"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 6), _values));
+      t_index += 6;
+      continue;
+    }
+    if(str.substr(t_index, 4) == "FROM"){
+      tokens.push_back(Token(t_index, str.substr(t_index, 4), _FROM));
+      t_index += 4;
+      continue;
+    }
+  }
+}
+
 bool parser::parse(string input){
   str = input;
   marker = 0;
--- parser.h
+++ parser.h
@@ -2,23 +2,31 @@
 #define PARSER_H
 
 #include<string>
+#include<vector>
 using namespace std;
 
-enum TokenType {_null = 0, _indentifier, _lparen, _rparen, _quote, _comma,
-                _assign, _space, _plus, _minus, _asterisk, _semicolon, _g, _l,
-                _e, _ge, _le, _ne}
-
 class parser{
+  enum TokenType {_null = 0, _indentifier, _lparen, _rparen, _comma, _assign,
+                  _space, _plus, _minus, _asterisk, _semicolon, _g, _l, _e, _ge,
+                  _le, _ne, _project, _rename,_open, _close, _write, _exit,
+                  _show, _create, _update, _insert, _delete, _varchar, _integer,
+                  _table, _primary, _key, _set, _where, _into, _values, _from,
+                  _relation, _literal};
+
   struct Token{
     size_t index;
     string field;
-    size_t size(){return field.size()};
-    size_t length(){return field.length()};
-  }
+    TokenType type;
+    size_t size() const {return field.size();}
+    size_t length() const {return field.length();}
+  Token(size_t new_index, string new_field, TokenType new_type)
+  : index(new_index), field(new_field), type(new_type) {}
+  };
 
-  }
+  vector<Token> tokens;
   string str;
-  size_t marker;
+  size_t s_index;
+  size_t t_index;
 
   char get(size_t index);
 
@@ -57,23 +65,24 @@ class parser{
   bool semicolon();
   bool literal(string lit);
   /*
-  bool command();
-  bool open();
-  bool close();
-  bool write();
-  bool exit();
-  bool show();
-  bool create();
-  bool update();
-  bool insert();
-  bool del();
-  bool typed_attr_list();
-  bool type();
-  bool integer();
+    bool command();
+    bool open();
+    bool close();
+    bool write();
+    bool exit();
+    bool show();
+    bool create();
+    bool update();
+    bool insert();
+    bool del();
+    bool typed_attr_list();
+    bool type();
+    bool integer();
   */
 
   void consumeWhitespace();
  public:
+  void lex(string input);
   bool parse(string input);
 };
 
