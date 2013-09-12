4ef4dc2f5a4a8df917337e5147ec8fb81e8ed1d0
gimlithepirate <gimlithepirate@gmail.com>
2013-09-12 10:34:22 -0600

    Completed draft of file writing
    This file write uses a class, that opens a file when you make the
    object, and then closes it when the object's destructor is called.

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,3 +1,72 @@
 #include "FileIO.h"
 
 using namespace std;
+
+fileIOOP(const Relation &R){ //constructor
+	attr = R.getColumns();
+	if(attr.empty()){
+		cout << "Cannot store a Relation with no attributes." << endl;
+	}
+	fileTarget = ofstream();
+	relationName = R.getName();
+	fileName = relationName + ".db";
+	fileTarget.open(fileName);
+	tups = R.getRows();
+	keys = R.getKeys();
+	for(int i; i++; i<attr.size()){ //This works because everything is a string. A little inefficient (checks length for int and double) but simple.
+		for(int j; j++; j<tups.size()){
+			if(varcharLength[i] < tups[j].getDataStrings()[i].length()){
+				varcharLength[i] = tups[j].getDataStrings()[i].length();
+			}
+		}
+	}
+}
+
+~fileIOOp(){
+	fileTarget.close();
+}
+
+void writeToFile(){
+	//Create the table
+	fileTarget <<"CREATE TABLE " << relationName << " (";
+	for(int i; i++; i<attr.size()){
+		fileTarget << atrr[i].getValue() << " ";
+		if(attr[i].getDataType() == dataTypes::str){
+			fileTarget << "VARCHAR(" << varcharLength[i] << ")";
+		}
+		else if(attr[i].getDataType() == dataTypes::in){
+			fileTarget << "INTEGER";
+		}
+		else if(attr[i].getDataType() == dataTypes::d){
+			fileTarget << "DOUBLE";
+		}
+		if(i != attr.size()-1){ fileTarget << ", ";}
+		else{fileTarget << ") ";}
+	}
+	if(!keys.empty()){
+		fileTarget << "PRIMARY KEY (";
+		for(int i; i++; i<keys.size()){
+			fileTarget << attr[keys[i]].getValue();
+			if(i != keys.size()-1){fileTarget << " ,";}
+			else{ fileTarget << ")";}
+		}
+	}
+	fileTarget << ";" << endl;
+	
+	//Put the Tuples in it
+	for(int i; i++; i<tups.size()){
+		fileTarget << "INSERT INTO " << relationName << " VALUES FROM (";
+		vector<string> tupData = tups[i].getDataStrings()
+		for(int j; j++; j < tupData.size()){
+			if(attr[j].getDataType == dataTypes::str){
+				fileTarget << "\"" << tupData[j] << "\"";
+			}
+			else{
+				fileTarget << tupData[j];
+			}
+			if(j != tupData.size() -1){fileTarget << ", ";}
+			else{fileTarget << ")";}
+		}
+		fileTarget << ";" << endl;
+	}
+}
--- FileIO.h
+++ FileIO.h
@@ -1,5 +1,22 @@
 #include <stdio>
+#include <iostream>
+#include <fstream>
 #include "Database.h"
 #include "Relation.h"
 #include "Attribute.h"
 #include "Tuple.h"
+
+class fileIOOp{
+	ofstream fileTarget;
+	string relationName;
+	string fileName;
+	vector<Tuple> tups;
+	vector<Attribute> attr;
+	vector<int> varcharLength;
+	vector<size_t> keys;
+	public:
+	//File is opened when constructor is called and deleted when the destructor  is called
+	fileIOOp(const Relation &R); //constructor
+	~fileIOOp(); //destructor
+	void writeToFile();
+}
