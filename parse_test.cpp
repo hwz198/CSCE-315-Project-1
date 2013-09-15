086ef987ecf9403cf8a3f6d1491dddac14866470
Vishal Anand <vish111@dhcp-10-201-83-19.tamulink.tamu.edu>
2013-09-15 14:34:51 -0500

    Added more Tests

--- parse_test.cpp
+++ parse_test.cpp
@@ -59,52 +59,210 @@ void showTableTest(){
   	string str = "SHOW animals animals;";
   	bool result = P.parse(str);
 	if(result){
-  	cout<<"Show Table test 1 failed"<<endl;
-  	return;
+  		cout<<"Show Table test 1 failed"<<endl;
+  		return;
   	}
   	
   	str = "Show animals;";
   	result = P.parse(str);
   	if(result!=0){
-  	cout<<"Show Table test 2 failed"<<endl;
-  	return;
+  		cout<<"Show Table test 2 failed"<<endl;
+  		return;
   	}
   	
   	str = "Show animals;;";
   	result = P.parse(str);
   	if(result!=0){
-  	cout<<"Show Table test 3 failed"<<endl;
-  	return;
+  		cout<<"Show Table test 3 failed"<<endl;
+  		return;
   	}
   	
-  	str = "SHOW SHOW animals;";
+  	str = "SHOW SHOW & animals;";
   	result = P.parse(str);
   	if(result!=0){
-  	cout<<"Show Table test 4 failed"<<endl;
-  	return;
+ 	 	cout<<"Show Table test 4 failed"<<endl;
+  		return;
   	}
   	
   	str = "SHOW animals;";
   	result = P.parse(str);
   	if(result!=0){
-  	cout<<"All Show Table tests Passed"<<endl;
-  	return;
+  		cout<<"All Show Table tests Passed"<<endl;
+  		return;
   	}
   	
+}
+
+void insertTest(){
+
+	parser P;  
+  	string str = "INSERT INT animals VALUES FROM (\"Joe\", \"cat\", 4);";
+  	bool result = P.parse(str);
+	if(result!=0){
+  		cout<<"Insert Table test 1 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "INSERT INTO animals VALueS FROM (\"Joe\", \"cat\", 4);";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Insert Table test 2 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "INSERT INTO animals VALUES FROM (\"Joe\",, \"cat\", 4);";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Insert Table test 3 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "INSERT INTO animals VALUES FROM \"Joe\", \"cat\", 4);";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Insert Table test 4 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "INSERT INTO animals VALUES FROM (\"Joe\", \"cat\",);";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Insert Table test 5 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "INSERT INTO points VALUES FROM RELATION (select (z2 > 0) dots_to_points);";
+  	result = P.parse(str);
+	if(result!=1){
+  		cout<<"Insert Table test 6 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "INSERT INTO animals VALUES FROM (\"Joe\", \"cat\", 4);";
+  	result = P.parse(str);
+	if(result==1){
+  		cout<<"All Insert Table tests passed"<<endl;
+  		return;
+  	}
+
+}
+
+void exitTest(){
+
+	parser P;  
+  	string str = "EXITT;";
+  	bool result = P.parse(str);
+	if(result!=0){
+  		cout<<"Exit test 1 failed"<<endl;
+  		return;
+  	}
+
+	str = "EXIT;blah";		//Text after semicolon 
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Exit test 2 failed"<<endl;
+  		return;
+  	}
+  	str = "exit;";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Exit test 3 failed"<<endl;
+  		return;
+  	}
+
+	str = "EXIT;";
+  	result = P.parse(str);
+	if(result==1){
+  		cout<<"All Exit tests Passed"<<endl;
+  		return;
+  	}
+
+}
+
+void queryTest(){
+
+	parser P;
+	string str = "common_names <- project (name) (select (aname == name & akind != kind) (a * animals));"; //single & gives infinite loop
+	bool result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 1 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "good_and_bad_guys <- friends + enemies;";
+  	result = P.parse(str);
+	if(result!=1){
+  		cout<<"Query Test 2 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "high_hitters <- select (homeruns >= 40) baseball_players;";
+  	result = P.parse(str);
+	if(result!=1){
+  		cout<<"Query Test 3 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "product_test <- shapes ** colors;";
+	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 4 failed"<<endl;
+  		return;
+  	}
+
+	str = "bad_query <- project t) points;";
+	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 5 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "dots_to_points < rename (x2, y2, z2) dots;";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 6 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "dots_to_points <- rename (x2, y2 z2) dots;";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 7 failed"<<endl;
+  		return;
+  	}
+
+	str = "dots_to_points <- renamE (x2, y2 z2) dots;";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 8 failed"<<endl;
+  		return;
+  	}
+  	
+  	str = "dots_to_points <-& rename (x2, y2, z2) dots;";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"Query Test 7 failed"<<endl;
+  		return;
+  	}
+
+	str = "dots_to_points <- rename (x2, y2, z2) dots;";
+  	result = P.parse(str);
+	if(result!=0){
+  		cout<<"All Query Tests Passed."<<endl;
+  		return;
+  	}
 }
 
 
 
 
 int main(){
-  parser P;
-  
-  
-  string str2 = "common_names <- project (name) (select (aname == name && akind != kind) (a * animals));"; //single & gives infinite loop
   
   testCreateTable();
   showTableTest(); //Write and Close are similar, hence not tested separately. 
-  
+  insertTest();
+  exitTest();
+  queryTest();
   
   
  /* while(true){
