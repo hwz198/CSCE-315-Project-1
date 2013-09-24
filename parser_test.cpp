9c509c310b060a56e0d105f3f39031a1e38b8ddf
heydabop <heydabop@gmail.com>
2013-09-24 16:24:01 -0500

    forgot parser_test

---
+++ parser_test.cpp
@@ -0,0 +1,13 @@
+#include "Parser.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+  Parser P;
+  string str; //= "high_hitters <- select (homeruns >= 40) baseball_players;";
+  while(getline(cin, str)){
+    cout << "$$ " << str << endl;
+    cout << P.parse(str) << endl << endl;
+  }
+  return 0;
+}
